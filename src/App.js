ba44b0b89b41ac52dc88ab20c91ce486237aed01
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2023-10-05 14:28:02 +0600

    form input behaviour - add task btn, reset btn added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { TaskList } from "./components/TaskList";
 import "./App.css";
-import AddTask from "./components/AddTask";
+import { AddTask } from "./components/AddTask";
 
 function App() {
   return (
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,15 +1,54 @@
-import React from "react";
+import { useState } from "react";
+import "./AddTask.css";
 
-function AddTask() {
+export const AddTask = () => {
+  const [taskValue, setTaskValue] = useState("");
+  const [progress, setProgress] = useState(false);
+
+  const handleChange = (event) => {
+    setTaskValue(event.target.value);
+  };
+
+  const handleReset = () => {
+    setTaskValue("");
+    setProgress(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const task = {
+      id: Math.floor(Math.random() * 10000),
+      name: taskValue,
+      completed: Boolean(progress),
+    };
+    handleReset();
+    console.log(task);
+  };
+
   return (
     <section className="addtask">
-      <form>
-        <label htmlFor="task">Task Name:</label>
-        <input type="text" name="task" id="task" placeholder="Task Name" autoComplete="off" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="task"
+          id="task"
+          placeholder="Task Name"
+          autoComplete="off"
+          value={taskValue}
+        />
+        <select
+          onChange={(event) => setProgress(event.target.value)}
+          value={progress}
+        >
+          <option value="false">Pending</option>
+          <option value="true">Completed</option>
+        </select>
         <button type="submit">Add Task</button>
+        <span onClick={handleReset} className="reset">
+          Reset
+        </span>
       </form>
     </section>
   );
-}
-
-export default AddTask;
+};
